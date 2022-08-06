f6809654e09642f495a92cf2617c3e342b8d1af9
Daniel Bae <danibae91@icloud.com>
2022-08-05 20:41:37 -0700

    prepare heroku deployment

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import connectDB from "./config/db.js";
 import pinsRoute from "./routes/pinsRoute.js"; 
 import usersRoute from "./routes/usersRoute.js"; 
 const port = process.env.PORT || 8800; 
+import errorHandler from './middleware/errorMiddleware.js'; 
 
 connectDB(); 
 
@@ -13,6 +14,19 @@ const app = express();
 // Middleware
 app.use(express.json()); // parse JSON
 app.use(express.urlencoded({ extended: false })); 
+app.use(errorHandler); 
+
+// For Frontend (running production)
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build"))); 
+    app.get("*", (req, res) => [
+        res.sendFile(path.resolve(__dirname, "../", "client", "build", "index.html"))
+    ]); 
+} else {
+    app.get("/", (req, res) => {
+        res.send("Please set to production!"); 
+    }); 
+}; 
 
 // Sample Request on localhost:8800 
 app.get("/", (req, res) => {
