8142a242ac31995fc3d70dd910daf958be28c07d
Daniel Bae <danibae91@icloud.com>
2024-01-27 00:08:22 -0600

    continue code cleanup; remove unnecessary components and styles

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,4 @@
 export { default as Navbar } from "./Navbar";
-export { default as Menu } from "./Menu";
 export { default as MainMap } from "./MainMap";
 export { default as Footer } from "./Footer";
 export { default as Spinner } from "./Spinner"; 
--- client/src/constants/links.js
+++ client/src/constants/links.js
@@ -1,11 +1,6 @@
 export const links = [
   {
     id: 1,
-    text: "Home",
-    path: "/",
-  },
-  {
-    id: 2,
     text: "Map",
     path: "/map",
   },
